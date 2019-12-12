6b82bc76349b9dda426e5dee16363259f722f303
Shanmukh <200438227@student.georgianc.on.ca>
2019-12-12 16:07:15 -0500

    Updated procfile

--- application.py
+++ application.py
@@ -13,6 +13,7 @@ sslmode = "require"
 # Construct connection string
 conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
 conn = psycopg2.connect(conn_string) 
+
 print("Connection established")
 cursor = conn.cursor()
 cursor1 = conn.cursor()
